ec5e5e9ae847e09949ef853fd76c639c6e395392
Luc <27276350+Lo-o@users.noreply.github.com>
2020-05-28 15:36:05 +0200

    Change path

--- office_project/urls.py
+++ office_project/urls.py
@@ -25,6 +25,6 @@ router.register(r"TriviaQuestions", views.TriviaQuestionViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include(router.urls)),
+    path("^service/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
