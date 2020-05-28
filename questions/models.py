c877453a822e4381f8fe6dcb2bafd53f17349d60
Luc <27276350+Lo-o@users.noreply.github.com>
2020-05-28 14:45:38 +0200

    ArrayField for options

--- questions/models.py
+++ questions/models.py
@@ -5,4 +5,4 @@ from django.contrib.postgres.fields import ArrayField
 class TriviaQuestion(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     question = models.CharField(max_length=500, blank=True)
-    options = models.ArrayField(models.CharField(max_length=500, blank=True))
+    options = ArrayField(models.CharField(max_length=500, blank=True))
